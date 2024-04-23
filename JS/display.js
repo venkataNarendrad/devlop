7bb598650b51c9205c95ab710e2724a5bb5948a1
VenkatNarendrad <venkatanarendra2000@gmail.com>
2024-04-23 10:35:14 +0530

    my first commit

---
+++ JS/display.js
@@ -0,0 +1,11 @@
+function displayTable(x)
+{
+
+for(let i=1;i<=10;i++)
+{
+
+ var result=i*x;
+ console.log(x+"*"+i+"="+result);
+}
+
+}
---
+++ JS/html.js
@@ -0,0 +1,9 @@
+function hide()
+{
+document.getElementById('h1').style.display='none';
+}
+
+function colorChange()
+{
+document.getElementById('para1').style.color='red';
+}
